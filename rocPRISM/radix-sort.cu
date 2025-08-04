71b3c3e5ef0ddcc4a2aae409111094ca9020d8d5
Sebastian Keller <sebastian.f.keller@gmail.com>
2025-08-04 18:29:08 +0200

    initial commit

---
+++ rocPRISM/radix-sort.cu
@@ -0,0 +1,56 @@
+/*
+ * AMD-GPU benchmarks
+ *
+ * Copyright (c) 2024 CSCS, ETH Zurich
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: MIT License
+ */
+
+/*! @file
+ * @brief Radix-sort performance test
+ *
+ * @author Sebastian Keller <sebastian.f.keller@gmail.com>
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <random>
+
+#include <thrust/device_vector.h>
+#include <thrust/sequence.h>
+#include <thrust/sort.h>
+
+#include "timing.cuh"
+
+int main(int argc, char** argv)
+{
+    using KeyType = uint64_t;
+    using ValueType = uint32_t;
+
+    int power   = argc > 1 ? std::stoi(argv[1]) : 25;
+    int numKeys = 1 << power;
+
+    std::vector<KeyType> hostKeys(numKeys);
+    {
+        std::mt19937 gen;
+        std::uniform_int_distribution<KeyType> dist(0, std::numeric_limits<KeyType>::max());
+        std::generate(hostKeys.begin(), hostKeys.end(), [&](){ return dist(gen); });
+    }
+
+    thrust::device_vector<KeyType> keys = hostKeys;
+    thrust::device_vector<ValueType> ordering(numKeys);
+    thrust::sequence(ordering.begin(), ordering.end(), 0);
+
+    auto radixSort = [&]()
+    {
+        thrust::sort_by_key(keys.begin(), keys.end(), ordering.begin());
+    };
+    float t_radixSort = timeGpu(radixSort);
+
+    size_t numBytesMoved = 2 * numKeys * (sizeof(KeyType) + sizeof(ValueType));
+    std::cout << "radix sort time for " << numKeys << " key-value pairs: " << t_radixSort / 1000 << " s"
+        << ", bandwidth: " << float(numBytesMoved) / t_radixSort / 1000 << " MiB/s" << std::endl;
+
+    return 0;
+}
---
+++ rocPRISM/timing.cuh
@@ -0,0 +1,66 @@
+/*
+ * AMD-GPU benchmarks
+ *
+ * Copyright (c) 2024 CSCS, ETH Zurich
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: MIT License
+ */
+
+/*! @file
+ * @brief GPU timing utility
+ *
+ * @author Sebastian Keller <sebastian.f.keller@gmail.com>
+ */
+
+#pragma once
+
+#include <chrono>
+#include "cuda_runtime.hpp"
+
+inline void checkErr(cudaError_t err, const char* filename, int lineno, const char* funcName)
+{
+    if (err != cudaSuccess)
+    {
+        const char* errName = cudaGetErrorName(err);
+        const char* errStr  = cudaGetErrorString(err);
+        fprintf(stderr, "CUDA Error at %s:%d. Function %s returned err %d: %s - %s\n", filename, lineno, funcName, err,
+                errName, errStr);
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define checkGpuErrors(errcode) checkErr((errcode), __FILE__, __LINE__, #errcode)
+
+//! @brief time a generic unary function
+template<class F>
+float timeGpu(F&& f)
+{
+    cudaEvent_t start, stop;
+    checkGpuErrors(cudaEventCreate(&start));
+    checkGpuErrors(cudaEventCreate(&stop));
+
+    checkGpuErrors(cudaEventRecord(start, cudaStreamDefault));
+
+    f();
+
+    checkGpuErrors(cudaEventRecord(stop, cudaStreamDefault));
+    cudaEventSynchronize(stop);
+
+    float t0;
+    checkGpuErrors(cudaEventElapsedTime(&t0, start, stop));
+
+    checkGpuErrors(cudaEventDestroy(start));
+    checkGpuErrors(cudaEventDestroy(stop));
+
+    return t0;
+}
+
+template<class F>
+float timeCpu(F&& f)
+{
+    auto t0 = std::chrono::high_resolution_clock::now();
+    f();
+    auto t1 = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<float>(t1 - t0).count();
+}
